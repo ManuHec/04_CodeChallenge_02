e61a00be5c1ce9011dea77f4b63776770837ebe1
Manuela <ellaheckenberger@gmail.com>
2021-11-19 23:17:57 +0100

    challenge_02_fertig

--- challenge_02.js
+++ challenge_02.js
@@ -16,3 +16,37 @@ Alter 13-17 // Cola
 
 /* Getränke-Challenge */
 
+let vorName, alter;
+
+vorName =prompt("Bitte, geben sie ihren Vornamen ein!");
+alter =prompt("Bitte, geben sie ihr alter ein!");
+
+console.log (vorName + " " + alter); 
+
+
+   if 
+        (alter <= 5) 
+{   console.log(vorName  + " trinkt Milch!");
+
+   }  else if 
+        (alter >=6 && alter <= 12)
+{   console.log(vorName + " trinkt Saft!");
+
+    } else if  
+        (alter >= 13 && alter <=17)  
+{   console.log(vorName + " tinkt Cola!");
+
+    } else 
+        (alter >= 18);
+{   console.log(vorName + " trinkt Wein!");
+
+ }  
+     
+
+
+
+
+
+
+
+
